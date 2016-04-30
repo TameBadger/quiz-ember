2b0703baced4ce51a5ad2f24cc162a1f2b868ee6
Rupert Klopper <tamebadger@protonmail.com>
2016-04-30 14:19:06 +0200

    Removed controller test as it has no state management or significant purpose

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -1,8 +1,6 @@
 import { moduleFor, test } from 'ember-qunit'
 
 moduleFor('adapter:application', 'Unit | Adapter | application', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
 })
 
 test('adapter initializes', function (assert) {
