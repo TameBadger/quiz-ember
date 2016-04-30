cfa823d8e6e2f2a572c8ab6cbb577e7b37f52c1e
Rupert Klopper <tamebadger@protonmail.com>
2016-04-30 17:26:41 +0200

    update travis variables, secure also encodes variable name, and more deploy script mods

--- deploy.sh
+++ deploy.sh
@@ -38,6 +38,7 @@ doCompile
 # Now let's go have some fun with the cloned repo
 cd out
 ls -1 | wc -l
+ls -l
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -49,7 +50,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add index.html
+git add assets
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
