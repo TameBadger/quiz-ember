4a77b0e5a69e84439b3e31e4fec6e354be29df5d
Rupert Klopper <tamebadger@protonmail.com>
2016-04-30 14:59:02 +0200

    Test build with files from master

---
+++ app/services/fake.js
@@ -0,0 +1,38 @@
+import Ember from 'ember'
+
+const { Service, A, inject: { service } } = Ember
+
+export default Service.extend({
+
+  init(...args) {
+    this._super(args)
+    this.set('statements', A())
+  },
+
+  store: service(),
+
+  statements: A(),
+
+  loadFakeStatement() {
+    this.get('statements').addObject(Ember.Object.create({
+      title: 'Statement 1',
+      option: 'A'
+    }))
+  },
+
+  loadRealStatements() {
+    this.get('store')
+    .findAll('statement')
+    .then(statements=> {
+      this.get('statements').addObjects(statements)
+    })
+  },
+
+  createRealStatement() {
+    this.get('store').createRecord('statement', {
+      title: 'Statement 1',
+      option: 'A'
+    })
+  }
+
+})
