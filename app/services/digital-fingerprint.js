5986e97eb38fcec7508f38d4569cb808da23dcac
Rupert Klopper <tamebadger@protonmail.com>
2016-04-30 15:09:30 +0200

    removed clientjs fingerprinting library

--- app/services/digital-fingerprint.js
+++ app/services/digital-fingerprint.js
@@ -2,7 +2,6 @@ import Ember from 'ember'
 
 const { Service, inject: { service } } = Ember
 
-// import clientjs from 'clientjs'
 import fingerprintjs2 from 'fingerprintjs2'
 
 export default Service.extend({
@@ -11,7 +10,6 @@ export default Service.extend({
 
   init(...args) {
     this._super(args)
-
     this.set('fingerprintjs2', new fingerprintjs2())
     this.get('fingerprintjs2').get((hash, data)=> {
       this.set('collectivePrints.fingerprintjs2', hash)
@@ -27,15 +25,6 @@ export default Service.extend({
         this.set('currentPrintRecord', record)
       })
     })
-
-    // I initially tested both fingerprinting libraries, to compare accuracy
-    // clientjs was clearly more inaccurate, leaving this here to investigate
-    // this.set('clientjs',new ClientJS())
-    // const clientjsFingerprint = this.get('clientjs').getFingerprint()
-    // this.set('collectivePrints.clientjsbasic',clientjsFingerprint)
-    // const browserData = this.get('clientjs').getBrowserData()
-    // this.set('collectivePrintData.clientjs',browserData)
-    // console.log(this.get('collectivePrints'))
   },
 
   fingerprintjs2: null,
@@ -45,7 +34,5 @@ export default Service.extend({
   collectivePrints: Ember.Object.create({}),
 
   collectivePrintData: Ember.Object.create({})
-
-  // clientjs: null,
 
 })
