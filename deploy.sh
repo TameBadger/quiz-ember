2d1aebfb0e4497af7b37b94388663c030e4d7932
Rupert Klopper <tamebadger@protonmail.com>
2016-04-30 20:03:23 +0200

    another try for travis

--- deploy.sh
+++ deploy.sh
@@ -18,64 +18,34 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-echo $SSH_REPO
-
-ls -1 | wc -l
-
-git clone --quiet --branch=gh-pages $REPO out
-
-ls -1 | wc -l
-
-pwd
-
-cd out 
-
-pwd
-
-git status
-git branch
-
-cd ..
+mkdir out
 
 doCompile
-
-pwd
 
 cd out
 
 pwd
 
-echo "Checking git status"
-git status
+git init
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git add index.html
+git add assets
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-echo "Checking git branch"
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../dep_key.enc -out dep_key -d
+
 git branch
+git status
+git show-ref
 
-echo "exiting"
-
-exit 0
-
-# git config user.name "Travis CI"
-# git config user.email "$COMMIT_AUTHOR_EMAIL"
-# git add index.html
-# git add assets
-# git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-
-
-# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../dep_key.enc -out dep_key -d
-
-# git branch
-# git status
-# git show-ref
-
-# chmod 600 dep_key
-# eval `ssh-agent -s`
-# ssh-add dep_key
+chmod 600 dep_key
+eval `ssh-agent -s`
+ssh-add dep_key
 
 # # Now that we're all set up, we can push.
-# git push --force --quiet $SSH_REPO $TARGET_BRANCH 
+git push -fq $SSH_REPO $TARGET_BRANCH 
